8b92b37e9734fc2c227010828c531c09cfb4d2ff
Guilherme Cardoso <86062682+gccardosin@users.noreply.github.com>
2022-07-04 07:57:38 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseUrl: 'https://api-missoes-urbanas.herokuapp.com'
+    baseUrl: 'https://apiceet.herokuapp.com/'
 });
 
 export default api;
